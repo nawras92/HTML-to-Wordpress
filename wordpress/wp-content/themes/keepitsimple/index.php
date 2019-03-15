90bafcf275c25b68fb84b2cc94947a2c8ff8b27e
Nawras Ali <nawras@learnwithnaw.net>
2019-03-15 19:31:03 +0200

    add bloginfo('lang')

--- wordpress/wp-content/themes/keepitsimple/index.php
+++ wordpress/wp-content/themes/keepitsimple/index.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<html class="no-js" lang="<?php bloginfo("language"); ?>">
 <head>
 
    <!--- Basic Page Needs
