44a8e9094cf4f8c9be26b64fe743e7c4d2392934
Nawras Ali <nawras@learnwithnaw.net>
2019-04-09 17:36:18 +0300

    add widget class to sidebar widgets

--- wordpress/wp-content/themes/keepitsimple/functions.php
+++ wordpress/wp-content/themes/keepitsimple/functions.php
@@ -1,36 +1,18 @@
 <?php
 
+// Register KeepItSimple Widgets
 function keepitsimple_register_sidebars(){
-  $sidebar1 = array(
+  $sidebar = array(
              "name" => "Main Sidebar",
-             "id"   => "sidebar1",
-             "before_widget" => "<div>",
+             "id"   => "sidebar",
+             "before_widget" => "<div class='widget'>",
              "after_widget" => "</div>",
              "before_title" => "<h3>",
              "after_title" => "</h3>"
   );
 
-  $sidebar2 = array(
-             "name" => "Sidebar 2",
-             "id"   => "sidebar2",
-             "before_widget" => "<li>",
-             "after_widget" => "</li>",
-             "before_title" => "<h4>",
-             "after_title" => "</h4>"
-  );
+     register_sidebar($sidebar);
 
-  $sidebar3 = array(
-             "name" => "Sidebar 3",
-             "id"   => "sidebar3",
-             "before_widget" => "<li>",
-             "after_widget" => "</li>",
-             "before_title" => "<h4>",
-             "after_title" => "</h4>"
-  );
-
-     register_sidebar($sidebar1);
-     register_sidebar($sidebar2);
-     register_sidebar($sidebar3);
 
 }
    add_action('widgets_init','keepitsimple_register_sidebars');
