b487ccdcaa37789183b1fe44bcc8c1f81b523e18
Nawras Ali <nawras@learnwithnaw.net>
2019-04-10 18:43:33 +0300

    register footer widgets

--- wordpress/wp-content/themes/keepitsimple/functions.php
+++ wordpress/wp-content/themes/keepitsimple/functions.php
@@ -13,6 +13,42 @@ function keepitsimple_register_sidebars(){
 
      register_sidebar($sidebar);
 
+$footer1 = array(
+                "name" => "footer 1",
+                "id"   => "footer1",
+                "before_widget" => "<div>",
+                "after_widget" => "</div>",
+                "before_title" => "<h3>",
+                "after_title" => "</h3>"
+     );
+
+     register_sidebar($footer1);
+
+
+     $footer2 = array(
+                     "name" => "footer 2",
+                     "id"   => "footer2",
+                     "before_widget" => "<div>",
+                     "after_widget" => "</div>",
+                     "before_title" => "<h3>",
+                     "after_title" => "</h3>"
+          );
+
+    register_sidebar($footer2);
+
+
+    $footer3 = array(
+                         "name" => "footer 3",
+                         "id"   => "footer3",
+                         "before_widget" => "<div>",
+                         "after_widget" => "</div>",
+                         "before_title" => "<h3>",
+                         "after_title" => "</h3>"
+              );
+
+    register_sidebar($footer3);
+
+
 
 }
    add_action('widgets_init','keepitsimple_register_sidebars');
