db727984e70a363f9500712967c99c94dbf71538
Nawras Ali <nawras@learnwithnaw.net>
2019-04-10 17:23:57 +0300

    remove social icons menu

--- wordpress/wp-content/themes/keepitsimple/footer.php
+++ wordpress/wp-content/themes/keepitsimple/footer.php
@@ -4,17 +4,6 @@
 
    <div class="row">
 
-     <div class="twelve columns">
-     <ul class="social-links">
-            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-            <li><a href="#"><i class="fa fa-github-square"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fa fa-flickr"></i></a></li>
-            <li><a href="#"><i class="fa fa-skype"></i></a></li>
-         </ul>
-     </div>
 
       <div class="six columns info">
 
